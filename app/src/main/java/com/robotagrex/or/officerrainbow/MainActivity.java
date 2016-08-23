7590e8ff2ccfd0e3370e482170febf1e15979958
mainphrame <mainphrame@hotmail.com>
2016-08-23 17:30:21 -0400

    toast

--- app/src/main/java/com/robotagrex/or/officerrainbow/MainActivity.java
+++ app/src/main/java/com/robotagrex/or/officerrainbow/MainActivity.java
@@ -1,19 +1,31 @@
 package com.robotagrex.or.officerrainbow;
 
 import android.content.Intent;
-import android.net.Uri;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText ed1,ed2;
+    public static final String MyPREFERENCES = "MyPrefs" ;
+    public static final String FirstName = "firstnameKey";
+    public static final String LastName = "lastnameKey";
+    SharedPreferences sharedpreferences;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ed1=(EditText)findViewById(R.id.editText);
+        ed2=(EditText)findViewById(R.id.editText2);
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
@@ -24,6 +36,15 @@ public class MainActivity extends AppCompatActivity {
         buttonfirstlast.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View view) {
+                String firstname  = ed1.getText().toString();
+                String lastname  = ed2.getText().toString();
+
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+
+                editor.putString(FirstName, firstname);
+                editor.putString(LastName, lastname);
+                editor.apply();
+                Toast.makeText(MainActivity.this,"Thanks",Toast.LENGTH_LONG).show();
                 Intent qoneintent = new Intent(MainActivity.this, Main2Activity.class);
                 startActivity(qoneintent);
             }
