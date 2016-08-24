42f80d80166a9aeb44ef857b3ef95073eb6a0e09
mainphrame <mainphrame@hotmail.com>
2016-08-24 18:55:53 -0400

    email and sms activity screen

--- app/src/main/java/com/robotagrex/or/officerrainbow/Main3Activity.java
+++ app/src/main/java/com/robotagrex/or/officerrainbow/Main3Activity.java
@@ -3,7 +3,10 @@ package com.robotagrex.or.officerrainbow;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -22,6 +25,40 @@ public class Main3Activity extends AppCompatActivity {
     public static final String smskey3 = "sms3Key";
     SharedPreferences sharedpreferences;
 
+    static final int PICK_CONTACT_REQUEST = 1;  // The request code
+
+    private void pickContact() {
+        Intent pickContactIntent = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
+        pickContactIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE); // Show user only contacts w/ phone numbers
+        startActivityForResult(pickContactIntent, PICK_CONTACT_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request it is that we're responding to
+        // Make sure the request was successful
+        if (requestCode == PICK_CONTACT_REQUEST) if (resultCode == RESULT_OK) {
+            // Get the URI that points to the selected contact
+            Uri contactUri = data.getData();
+            // We only need the NUMBER column, because there will be only one row in the result
+            String[] projection = {ContactsContract.CommonDataKinds.Phone.NUMBER};
+
+            // Perform the query on the contact to get the NUMBER column
+            // We don't need a selection or sort order (there's only one result for the given URI)
+            // CAUTION: The query() method should be called from a separate thread to avoid blocking
+            // your app's UI thread. (For simplicity of the sample, this code doesn't do that.)
+            // Consider using CursorLoader to perform the query.
+            Cursor cursor = getContentResolver()
+                    .query(contactUri, projection, null, null, null);
+            cursor.moveToFirst();
+
+            // Retrieve the phone number from the NUMBER column
+            int column = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+            String number = cursor.getString(column);
+
+            // Do something with the phone number...
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +71,6 @@ public class Main3Activity extends AppCompatActivity {
         ed4=(EditText)findViewById(R.id.editText2);
         ed5=(EditText)findViewById(R.id.editText5);
         ed6=(EditText)findViewById(R.id.editText6);
-
-
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
