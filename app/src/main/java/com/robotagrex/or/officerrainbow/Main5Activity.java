558fff79216dd16c6d2ed91dab4d53d551625ec3
mainphrame <mainphrame@hotmail.com>
2016-08-25 19:28:14 -0400

    main 2 activity to manifest

--- app/src/main/java/com/robotagrex/or/officerrainbow/Main5Activity.java
+++ app/src/main/java/com/robotagrex/or/officerrainbow/Main5Activity.java
@@ -6,72 +6,14 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class Main5Activity extends AppCompatActivity {
 
-
-
-
     public static final String MyPREFERENCES = "MyPrefs" ;
-    public static final String DEBUG_TAG = "Officer Rainbow";
     SharedPreferences sharedpreferences;
-
-    // Given a URL, establishes an HttpUrlConnection and retrieves
-// the web page content as a InputStream, which it returns as
-// a string.
-    private String downloadUrl(String myurl) throws IOException {
-        InputStream is = null;
-        // Only display the first 50 characters of the retrieved
-        // web page content.
-        int len = 50;
-
-        try {
-            URL url = new URL("https://www.nova-testing.com/");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setReadTimeout(10000 /* milliseconds */);
-            conn.setConnectTimeout(15000 /* milliseconds */);
-            conn.setRequestMethod("GET");
-            conn.setDoInput(true);
-            // Starts the query
-            conn.connect();
-            int response = conn.getResponseCode();
-            Log.d(DEBUG_TAG, "The response is: " + response);
-            is = conn.getInputStream();
-
-            // Convert the InputStream into a string
-            String contentAsString = readIt(is, len);
-            return contentAsString;
-
-            // Makes sure that the InputStream is closed after the app is
-            // finished using it.
-        } finally {
-            if (is != null) {
-                is.close();
-            }
-        }
-    }
-
-    // Reads an InputStream and converts it to a String.
-    public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
-        Reader reader = null;
-        reader = new InputStreamReader(stream, "UTF-8");
-        char[] buffer = new char[len];
-        reader.read(buffer);
-        return new String(buffer);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,7 +23,7 @@ public class Main5Activity extends AppCompatActivity {
         //http://stackoverflow.com/questions/3275467/why-cant-i-set-text-to-an-android-textview
         TextView notification_msg = (TextView) findViewById(R.id.textView3);
         assert notification_msg != null;
-        notification_msg.setText("test");
+        notification_msg.setText(R.string.app_name);
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
