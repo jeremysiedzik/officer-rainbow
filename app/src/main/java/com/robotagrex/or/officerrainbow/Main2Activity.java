42ba483504debc9c2ad86b121a96c279517e6b08
mainphrame <mainphrame@hotmail.com>
2016-08-25 13:48:27 -0400

    contact picker - button align

--- app/src/main/java/com/robotagrex/or/officerrainbow/Main2Activity.java
+++ app/src/main/java/com/robotagrex/or/officerrainbow/Main2Activity.java
@@ -3,8 +3,11 @@ package com.robotagrex.or.officerrainbow;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
@@ -12,7 +15,7 @@ import android.widget.EditText;
 
 public class Main2Activity extends AppCompatActivity {
 
-    EditText ed1,ed2,ed3,ed4,ed5,ed6;
+    EditText ed1,ed2,ed3,ed4,ed5,ed6,current_ed;
     public static final String MyPREFERENCES = "MyPrefs" ;
     public static final String emailkey1 = "email1Key";
     public static final String emailkey2 = "email2Key";
@@ -21,6 +24,8 @@ public class Main2Activity extends AppCompatActivity {
     public static final String smskey2 = "sms2Key";
     public static final String smskey3 = "sms3Key";
     SharedPreferences sharedpreferences;
+
+    static final int PICK_CONTACT_REQUEST = 1;  // The request code
 
 
     @Override
@@ -42,6 +47,15 @@ public class Main2Activity extends AppCompatActivity {
         Button buttonfirstlast = (Button) findViewById(R.id.buttonlast);
         assert buttonfirstlast != null;
 
+        Button contactbutton1 = (Button) findViewById(R.id.buttoncontact1);
+        assert contactbutton1 != null;
+
+        Button contactbutton2 = (Button) findViewById(R.id.buttoncontact2);
+        assert contactbutton2 != null;
+
+        Button contactbutton3 = (Button) findViewById(R.id.buttoncontact3);
+        assert contactbutton3 != null;
+
         sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
 
         String fillemail1 = sharedpreferences.getString("email1Key", "");
@@ -57,6 +71,41 @@ public class Main2Activity extends AppCompatActivity {
         ed4.setText(fillsms1);
         ed5.setText(fillsms2);
         ed6.setText(fillsms3);
+
+        contactbutton1.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick (View view) {
+                current_ed=(EditText)findViewById(R.id.editText2);
+                Intent pickContactIntent1 = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
+                pickContactIntent1.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE); // Show user only contacts w/ phone numbers
+                startActivityForResult(pickContactIntent1, PICK_CONTACT_REQUEST);
+            }
+        });
+
+        contactbutton2.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick (View view) {
+                current_ed=(EditText)findViewById(R.id.editText5);
+                Intent pickContactIntent2 = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
+                pickContactIntent2.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE); // Show user only contacts w/ phone numbers
+                startActivityForResult(pickContactIntent2, PICK_CONTACT_REQUEST);
+            }
+        });
+
+        contactbutton3.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick (View view) {
+                current_ed=(EditText)findViewById(R.id.editText6);
+                Intent pickContactIntent3 = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
+                pickContactIntent3.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE); // Show user only contacts w/ phone numbers
+                startActivityForResult(pickContactIntent3, PICK_CONTACT_REQUEST);
+            }
+        });
+
+
 
         buttonfirstlast.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -77,9 +126,38 @@ public class Main2Activity extends AppCompatActivity {
                 editor.putString(smskey2, sms2);
                 editor.putString(smskey3, sms3);
                 editor.apply();
-                Intent qoneintent = new Intent(Main2Activity.this, Main3Activity.class);
+                Intent qoneintent = new Intent(Main2Activity.this, MainActivity.class);
                 startActivity(qoneintent);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request it is that we're responding to
+        // Make sure the request was successful
+        if (requestCode == PICK_CONTACT_REQUEST) if (resultCode == RESULT_OK) {
+            // Get the URI that points to the selected contact
+            Uri contactUri = data.getData();
+            // We only need the NUMBER column, because there will be only one row in the result
+            String[] projection = {ContactsContract.CommonDataKinds.Phone.NUMBER};
+
+            // Perform the query on the contact to get the NUMBER column
+            // We don't need a selection or sort order (there's only one result for the given URI)
+            // CAUTION: The query() method should be called from a separate thread to avoid blocking
+            // your app's UI thread. (For simplicity of the sample, this code doesn't do that.)
+            // Consider using CursorLoader to perform the query.
+            Cursor cursor = getContentResolver()
+                    .query(contactUri, projection, null, null, null);
+            assert cursor != null;
+            cursor.moveToFirst();
+
+            // Retrieve the phone number from the NUMBER column
+            int column = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+            String number = cursor.getString(column);
+
+            // Do something with the phone number...
+            current_ed.setText(number);
+        }
     }
 }
