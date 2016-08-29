36840c00189f39256f3c0e73bf4f3f95cf2dea8b
mainphrame <mainphrame@hotmail.com>
2016-08-29 13:10:00 -0400

    main 5 network test

--- app/src/main/java/com/robotagrex/or/officerrainbow/Main5Activity.java
+++ app/src/main/java/com/robotagrex/or/officerrainbow/Main5Activity.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -11,6 +12,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
 
 public class Main5Activity extends AppCompatActivity {
 
@@ -18,7 +22,7 @@ public class Main5Activity extends AppCompatActivity {
     SharedPreferences sharedpreferences;
 
     // URL Address
-    String url = "http://www.robotagrex.com";
+    String url = "http://ec2-52-42-215-71.us-west-2.compute.amazonaws.com/onsite-dropmessage.txt";
     ProgressDialog mProgressDialog;
 
     @Override
@@ -27,6 +31,43 @@ public class Main5Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main_5);
 
         Button titlebutton = (Button) findViewById(R.id.titlebutton);
+
+        // Title AsyncTask
+        class Title extends AsyncTask<Void, Void, Void> {
+            String title;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                mProgressDialog = new ProgressDialog(Main5Activity.this);
+                mProgressDialog.setTitle("Android Basic JSoup Tutorial");
+                mProgressDialog.setMessage("Loading...");
+                mProgressDialog.setIndeterminate(false);
+                mProgressDialog.show();
+            }
+
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+                    // Connect to the web site
+                    Document document = Jsoup.connect(url).get();
+                    // Get the html document title
+                    title = document.title();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void result) {
+                // Set title into TextView
+                TextView txttitle = (TextView) findViewById(R.id.titletxt);
+                assert txttitle != null;
+                txttitle.setText(title);
+                mProgressDialog.dismiss();
+            }
+        }
 
         TextView notification_msg = (TextView) findViewById(R.id.textView3);
         assert notification_msg != null;
@@ -39,6 +80,15 @@ public class Main5Activity extends AppCompatActivity {
         assert buttontest != null;
 
         sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+
+        // Capture button click
+        assert titlebutton != null;
+        titlebutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                // Execute Title AsyncTask
+                new Title().execute();
+            }
+        });
 
         buttontest.setOnClickListener(new View.OnClickListener() {
             @Override
