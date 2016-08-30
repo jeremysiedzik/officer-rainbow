81e4393b7deb5d11585da91b350f1d97cbf54aaa
mainphrame <mainphrame@hotmail.com>
2016-08-30 16:43:15 -0400

    main 6 choose website

--- app/src/main/java/com/robotagrex/or/officerrainbow/Main7Activity.java
+++ app/src/main/java/com/robotagrex/or/officerrainbow/Main7Activity.java
@@ -3,23 +3,15 @@ package com.robotagrex.or.officerrainbow;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class Main7Activity extends AppCompatActivity {
 
     public static final String MyPREFERENCES = "MyPrefs" ;
-    private DatePicker datePicker;
-    private Calendar calendar;
-    private TextView dateView;
-    private int year, month, day;
     SharedPreferences sharedpreferences;
 
 
