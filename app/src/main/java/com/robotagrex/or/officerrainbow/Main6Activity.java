9ed697831072edad6a53e87c2438342d31981b20
mainphrame <mainphrame@hotmail.com>
2016-08-30 14:30:18 -0400

    main 6 choose website

--- app/src/main/java/com/robotagrex/or/officerrainbow/Main6Activity.java
+++ app/src/main/java/com/robotagrex/or/officerrainbow/Main6Activity.java
@@ -8,12 +8,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ToggleButton;
 
 public class Main6Activity extends AppCompatActivity {
 
     public static final String MyPREFERENCES = "MyPrefs" ;
-
     SharedPreferences sharedpreferences;
+    ToggleButton toggleButton, toggleButton2, toggleButton3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +28,11 @@ public class Main6Activity extends AppCompatActivity {
         assert buttonnext != null;
 
         sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        toggleButton.setChecked(sharedpreferences.getBoolean("jams_state", true));
+        toggleButton2.setChecked(sharedpreferences.getBoolean("nova_state", true));
+        toggleButton3.setChecked(sharedpreferences.getBoolean("onsite_state", true));
+
+
 
         buttonnext.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +40,51 @@ public class Main6Activity extends AppCompatActivity {
 
                 Intent qoneintent = new Intent(Main6Activity.this, MainActivity.class);
                 startActivity(qoneintent);
+            }
+        });
+
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                if (toggleButton.isChecked())
+                {
+                    SharedPreferences.Editor editor = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE).edit();
+                    editor.putBoolean("jams_state", true);
+                    editor.apply();
+                }
+                else
+                {
+                    SharedPreferences.Editor editor = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE).edit();
+                    editor.putBoolean("jams_state", false);
+                    editor.apply();
+                }
+
+                if (toggleButton2.isChecked())
+                {
+                    SharedPreferences.Editor editor = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE).edit();
+                    editor.putBoolean("nova_state", true);
+                    editor.apply();
+                }
+                else
+                {
+                    SharedPreferences.Editor editor = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE).edit();
+                    editor.putBoolean("nova_state", false);
+                    editor.apply();
+                }
+
+                if (toggleButton3.isChecked())
+                {
+                    SharedPreferences.Editor editor = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE).edit();
+                    editor.putBoolean("onsite_state", true);
+                    editor.apply();
+                }
+                else
+                {
+                    SharedPreferences.Editor editor = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE).edit();
+                    editor.putBoolean("onsite_state", false);
+                    editor.apply();
+                }
             }
         });
     }
