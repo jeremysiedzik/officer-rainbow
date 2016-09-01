43f07b59d706ad1bd7504dd0eb9feea11c9794d7
mainphrame <mainphrame@hotmail.com>
2016-09-01 15:35:47 -0400

    alarm strings typos and additions...spell much

--- app/src/main/java/com/robotagrex/or/officerrainbow/MyActivity.java
+++ app/src/main/java/com/robotagrex/or/officerrainbow/MyActivity.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 import java.util.Calendar;
 
@@ -19,6 +20,17 @@ public class MyActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        Button buttonnext = (Button) findViewById(R.id.buttonnext);
+        assert buttonnext != null;
+
+        buttonnext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent qoneintent = new Intent(MyActivity.this, MainActivity.class);
+                startActivity(qoneintent);
+            }
+        });
 
         /* Retrieve a PendingIntent that will perform a broadcast */
         Intent alarmIntent = new Intent(MyActivity.this, AlarmReceiver.class);
