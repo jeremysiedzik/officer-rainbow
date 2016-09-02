01160965822a1bc76d3552218c7b9e7905cc964f
mainphrame <mainphrame@hotmail.com>
2016-09-02 09:51:31 -0400

    alarm strings typos and additions...spell much

--- app/src/main/java/com/robotagrex/or/officerrainbow/MyActivity.java
+++ app/src/main/java/com/robotagrex/or/officerrainbow/MyActivity.java
@@ -79,8 +79,8 @@ public class MyActivity extends Activity {
         /* Set the alarm to start at 10:30 AM */
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(System.currentTimeMillis());
-        calendar.set(Calendar.HOUR_OF_DAY, 16);
-        calendar.set(Calendar.MINUTE, 30);
+        calendar.set(Calendar.HOUR_OF_DAY, 9);
+        calendar.set(Calendar.MINUTE, 55);
 
         /* Repeating on every 20 minutes interval */
         manager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
