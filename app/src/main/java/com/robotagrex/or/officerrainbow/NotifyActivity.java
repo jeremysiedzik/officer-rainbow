ea712a5aa29781aaf54f18574045e5d39faa0789
mainphrame <mainphrame@hotmail.com>
2016-09-06 10:39:02 -0400

    alarm bunch of changes

--- app/src/main/java/com/robotagrex/or/officerrainbow/NotifyActivity.java
+++ app/src/main/java/com/robotagrex/or/officerrainbow/NotifyActivity.java
@@ -1,9 +1,13 @@
 package com.robotagrex.or.officerrainbow;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 /**
@@ -21,6 +25,20 @@ public class NotifyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.alarm_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        Button buttonnext = (Button) findViewById(R.id.buttonnext);
+        assert buttonnext != null;
+
+        buttonnext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent qoneintent = new Intent(NotifyActivity.this, MainActivity.class);
+                startActivity(qoneintent);
+            }
+        });
     }
 
     @Override
