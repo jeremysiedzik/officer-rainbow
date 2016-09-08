c5c6dc5793f8d9ab4b43dae70fa8e230194a77da
mainphrame <mainphrame@hotmail.com>
2016-09-08 17:40:20 -0400

    remove alarm re-tool

---
+++ app/src/main/java/com/robotagrex/or/officerrainbow/JobSchedulerService.java
@@ -0,0 +1,20 @@
+package com.robotagrex.or.officerrainbow;
+
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+
+public class JobSchedulerService extends JobService {
+
+    @Override
+    public boolean onStartJob(JobParameters params) {
+
+        return false;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters params) {
+
+        return false;
+    }
+
+}
