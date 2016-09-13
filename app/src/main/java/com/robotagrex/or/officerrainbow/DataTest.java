e85b6d923ada0f167a30b66804f9290c89e7bb3c
mainphrame <mainphrame@hotmail.com>
2016-09-13 16:24:18 -0400

    cleanup

--- app/src/main/java/com/robotagrex/or/officerrainbow/DataTest.java
+++ app/src/main/java/com/robotagrex/or/officerrainbow/DataTest.java
@@ -23,7 +23,7 @@ public class DataTest extends AppCompatActivity {
     SharedPreferences sharedpreferences;
 
     // URL Address
-    String url = "http://feed.robotagrex.com/sendmail.php?emailaddress=mainphrame@hotmail.com&emailmessage=hellodolly";
+    String url = "http://feed.robotagrex.com/aggregate.txt";
     ProgressDialog mProgressDialog;
 
     @Override
