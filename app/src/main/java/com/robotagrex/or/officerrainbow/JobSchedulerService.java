1cccc8957ed7882c9a462f25056f1d062161f23c
mainphrame <mainphrame@hotmail.com>
2016-09-14 15:05:28 -0400

    trigger data pull on schedule

--- app/src/main/java/com/robotagrex/or/officerrainbow/JobSchedulerService.java
+++ app/src/main/java/com/robotagrex/or/officerrainbow/JobSchedulerService.java
@@ -1,36 +1,20 @@
 package com.robotagrex.or.officerrainbow;
 
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
-import android.content.Context;
-import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Message;
-import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.Calendar;
 
 public class JobSchedulerService extends JobService {
 
-    public static final String TAG = "Officer Rainbow";
-    public static final int NOTIFICATION_ID = 1;
-    public static final String SEARCH_STRING = "PURPLE";
-    public static final String url = "http://ec2-52-42-215-71.us-west-2.compute.amazonaws.com/aggregate.txt";
-
     private Handler mJobHandler = new Handler( new Handler.Callback() {
+        public static final String TAG = "Officer Rainbow";
 
         @Override
         public boolean handleMessage(Message msg) {
@@ -43,30 +27,11 @@ public class JobSchedulerService extends JobService {
             if (alarm_time == 16) {
                 Log.i(TAG, "JobService running - it's 5am");
 
-                String urlString = url;
-
-                String result ="";
-
-                // Try to connect to the Google homepage and download content.
-                try {
-                    Log.i(TAG, "Calling loadFromNetwork");
-                    result = loadFromNetwork(urlString);
-                } catch (IOException e) {
-                    Log.i(TAG, getString(R.string.connection_error));
-                }
-
-                if (result.contains(SEARCH_STRING)) {
-                    sendNotification(getString(R.string.doodle_found));
-                    Log.i(TAG, "Found color!!");
-                } else {
-                    sendNotification(getString(R.string.no_doodle));
-                    Log.i(TAG, "No color found. :-(");
-                }
-
                 final AudioManager mAudioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
                 final int originalVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
                 mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC), 0);
 
+                //final MediaPlayer mPlayer = new MediaPlayer();
                 final MediaPlayer mPlayer = MediaPlayer.create(getApplicationContext(), getResources().getIdentifier("beep", "raw", getPackageName()));
                 mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
 
@@ -107,101 +72,4 @@ public class JobSchedulerService extends JobService {
         return false;
     }
 
-    private void sendNotification(String msg) {
-        NotificationManager mNotificationManager = (NotificationManager)
-                this.getSystemService(Context.NOTIFICATION_SERVICE);
-
-        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
-                new Intent(this, OfficerRainbow.class), 0);
-
-        NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(this)
-                        .setSmallIcon(R.drawable.ic_launcher)
-                        .setContentTitle(getString(R.string.doodle_alert))
-                        .setStyle(new NotificationCompat.BigTextStyle()
-                                .bigText(msg))
-                        .setContentText(msg);
-
-        mBuilder.setContentIntent(contentIntent);
-        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
-    }
-
-//
-// The methods below this line fetch content from the specified URL and return the
-// content as a string.
-//
-    /** Given a URL string, initiate a fetch operation. */
-    private String loadFromNetwork(String urlString) throws IOException {
-        InputStream stream = null;
-        String str ="";
-
-        try {
-            Log.i(TAG, "Calling downloadUrl");
-            stream = downloadUrl(urlString);
-            str = readIt(stream);
-        } finally {
-            if (stream != null) {
-                stream.close();
-            }
-        }
-        return str;
-    }
-
-    /**
-     * Given a string representation of a URL, sets up a connection and gets
-     * an input stream.
-     * @param urlString A string representation of a URL.
-     * @return An InputStream retrieved from a successful HttpURLConnection.
-     * @throws IOException
-     */
-
-    private InputStream downloadUrl(String urlString) throws IOException {
-
-        URL url = new URL(urlString);
-        Log.i(TAG, "Calling HttpURLConnection");
-        final HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setReadTimeout(10000 /* milliseconds */);
-        conn.setConnectTimeout(15000 /* milliseconds */);
-        conn.setRequestMethod("GET");
-        conn.setDoInput(true);
-        // Start the quer
-
-        class fetchurl extends AsyncTask<Void, Void, String> {
-            @Override
-            protected String doInBackground(Void... params) {
-                Log.i(TAG, "Calling conn.connect()");
-                try {
-                    conn.connect();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    return String.valueOf(conn.getInputStream());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                return null;
-            }
-
-        }
-        Log.i(TAG, "Calling fetchurl().execute()");
-        new fetchurl().execute();
-        return null;
-    }
-
-    /**
-     * Reads an InputStream and converts it to a String.
-     * @param stream InputStream containing HTML from www.google.com.
-     * @return String version of InputStream.
-     * @throws IOException
-     */
-    private String readIt(InputStream stream) throws IOException {
-
-        StringBuilder builder = new StringBuilder();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-        for(String line = reader.readLine(); line != null; line = reader.readLine())
-            builder.append(line);
-        reader.close();
-        return builder.toString();
-    }
 }
