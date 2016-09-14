801857a815423cb5a93ab87be37144da9c83e5a4
mainphrame <mainphrame@hotmail.com>
2016-09-14 16:33:12 -0400

    ui cleanup

--- app/src/main/java/com/robotagrex/or/officerrainbow/JobSchedulerService.java
+++ app/src/main/java/com/robotagrex/or/officerrainbow/JobSchedulerService.java
@@ -4,6 +4,7 @@ import android.app.job.JobParameters;
 import android.app.job.JobService;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Handler;
@@ -16,6 +17,8 @@ import java.util.Calendar;
 public class JobSchedulerService extends JobService {
 
     public static final String TAG = "Officer Rainbow";
+    public static final String MyPREFERENCES = "MyPrefs" ;
+    SharedPreferences sharedpreferences;
 
     private Handler mJobHandler = new Handler( new Handler.Callback() {
 
