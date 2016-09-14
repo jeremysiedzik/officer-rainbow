89474fd2bc64a0975ea7efec8eadf4c03f22a003
mainphrame <mainphrame@hotmail.com>
2016-09-14 11:51:41 -0400

    cleanup

--- app/src/main/java/com/robotagrex/or/officerrainbow/JobSchedulerService.java
+++ app/src/main/java/com/robotagrex/or/officerrainbow/JobSchedulerService.java
@@ -1,20 +1,42 @@
 package com.robotagrex.or.officerrainbow;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Calendar;
 
 public class JobSchedulerService extends JobService {
 
+    public static final String TAG = "Officer Rainbow";
+    // An ID used to post the notification.
+    public static final int NOTIFICATION_ID = 1;
+    // The string the app searches for in the Google home page content. If the app finds
+    // the string, it indicates the presence of a doodle.
+    public static final String SEARCH_STRING = "PURPLE";
+    // The Google home page URL from which the app fetches content.
+    // You can find a list of other Google domains with possible doodles here:
+    // http://en.wikipedia.org/wiki/List_of_Google_domains
+    public static final String URL = "http://ec2-52-42-215-71.us-west-2.compute.amazonaws.com/aggregate.txt";
+    //NotificationCompat.Builder builder;
+
     private Handler mJobHandler = new Handler( new Handler.Callback() {
-        public static final String TAG = "Officer Rainbow";
 
         @Override
         public boolean handleMessage(Message msg) {
@@ -23,12 +45,30 @@ public class JobSchedulerService extends JobService {
             int alarm_time = c.get(Calendar.HOUR_OF_DAY);
             Toast.makeText(getApplicationContext(), "JobService task running", Toast.LENGTH_SHORT).show();
             Log.i(TAG, "JobService idling");
-            Intent i = new Intent(JobSchedulerService.this, WebSitechecker.class);
-            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startService(i);
 
             if (alarm_time == 16) {
                 Log.i(TAG, "JobService running - it's 5am");
+
+                String urlString = URL;
+
+                String result ="";
+
+                // Try to connect to the Google homepage and download content.
+                try {
+                    result = loadFromNetwork(urlString);
+                } catch (IOException e) {
+                    Log.i(TAG, getString(R.string.connection_error));
+                }
+
+                // If the app finds the string "doodle" in the Google home page content, it
+                // indicates the presence of a doodle. Post a "Doodle Alert" notification.
+                if (result.contains(SEARCH_STRING)) {
+                    sendNotification(getString(R.string.doodle_found));
+                    Log.i(TAG, "Found color!!");
+                } else {
+                    sendNotification(getString(R.string.no_doodle));
+                    Log.i(TAG, "No color found. :-(");
+                }
 
                 final AudioManager mAudioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
                 final int originalVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
@@ -73,6 +113,81 @@ public class JobSchedulerService extends JobService {
     public boolean onStopJob( JobParameters params ) {
         mJobHandler.removeMessages( 1 );
         return false;
+    }
+
+    private void sendNotification(String msg) {
+        NotificationManager mNotificationManager = (NotificationManager)
+                this.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, OfficerRainbow.class), 0);
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_launcher)
+                        .setContentTitle(getString(R.string.doodle_alert))
+                        .setStyle(new NotificationCompat.BigTextStyle()
+                                .bigText(msg))
+                        .setContentText(msg);
+
+        mBuilder.setContentIntent(contentIntent);
+        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+    }
+
+//
+// The methods below this line fetch content from the specified URL and return the
+// content as a string.
+//
+    /** Given a URL string, initiate a fetch operation. */
+    private String loadFromNetwork(String urlString) throws IOException {
+        InputStream stream = null;
+        String str ="";
+
+        try {
+            stream = downloadUrl(urlString);
+            str = readIt(stream);
+        } finally {
+            if (stream != null) {
+                stream.close();
+            }
+        }
+        return str;
+    }
+
+    /**
+     * Given a string representation of a URL, sets up a connection and gets
+     * an input stream.
+     * @param urlString A string representation of a URL.
+     * @return An InputStream retrieved from a successful HttpURLConnection.
+     * @throws IOException
+     */
+    private InputStream downloadUrl(String urlString) throws IOException {
+
+        URL url = new URL(urlString);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setReadTimeout(10000 /* milliseconds */);
+        conn.setConnectTimeout(15000 /* milliseconds */);
+        conn.setRequestMethod("GET");
+        conn.setDoInput(true);
+        // Start the query
+        conn.connect();
+        return conn.getInputStream();
+    }
+
+    /**
+     * Reads an InputStream and converts it to a String.
+     * @param stream InputStream containing HTML from www.google.com.
+     * @return String version of InputStream.
+     * @throws IOException
+     */
+    private String readIt(InputStream stream) throws IOException {
+
+        StringBuilder builder = new StringBuilder();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+        for(String line = reader.readLine(); line != null; line = reader.readLine())
+            builder.append(line);
+        reader.close();
+        return builder.toString();
     }
 
 }
