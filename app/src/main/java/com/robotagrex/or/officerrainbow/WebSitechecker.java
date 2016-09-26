500e4354acfc94cb4f76eddc5f9e32f15beb8b58
mainphrame <mainphrame@hotmail.com>
2016-09-26 16:59:29 -0400

    main ui holy changes batman

--- app/src/main/java/com/robotagrex/or/officerrainbow/WebSitechecker.java
+++ app/src/main/java/com/robotagrex/or/officerrainbow/WebSitechecker.java
@@ -71,7 +71,7 @@ public class WebSitechecker extends IntentService {
                 this.getSystemService(Context.NOTIFICATION_SERVICE);
     
         PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
-            new Intent(this, OfficerRainbow.class), 0);
+            new Intent(this, UserInterface.class), 0);
 
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
