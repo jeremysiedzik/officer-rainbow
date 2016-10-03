3620e3bc9e5c590e9d0bd703f3ddc8736ba5add9
mainphrame <mainphrame@hotmail.com>
2016-10-03 13:52:50 -0400

    image browser intent system out iteration message

--- app/src/main/java/com/robotagrex/or/officerrainbow/UI.java
+++ app/src/main/java/com/robotagrex/or/officerrainbow/UI.java
@@ -322,8 +322,8 @@ public class UI extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 stopaudio(getApplication());
-                Intent qoneintent = new Intent(UI.this, ImageStore.class);
-                startActivity(qoneintent);
+                //Intent qoneintent = new Intent(UI.this, ImageStore.class);
+                //startActivity(qoneintent);
             }
         });
 
