3919c03b164b5a85da83c8177d61d1a245a8aeb6
mainphrame <mainphrame@hotmail.com>
2016-10-08 12:37:24 -0400

    UI fun

--- app/src/main/java/com/robotagrex/or/officerrainbow/UI.java
+++ app/src/main/java/com/robotagrex/or/officerrainbow/UI.java
@@ -97,13 +97,13 @@ public class UI extends AppCompatActivity {
         color_choice_2 = (TextView)findViewById(R.id.color_choice_2);
         String fillcolor2 = sharedpreferences.getString("color2Key", "");
         if((fillcolor2.length() != 0)) {
-            color_choice_2.setText(fillcolor1);
+            color_choice_2.setText(fillcolor2);
         }
 
         color_choice_3 = (TextView)findViewById(R.id.color_choice_3);
         String fillcolor3 = sharedpreferences.getString("color3Key", "");
         if((fillcolor3.length() != 0)) {
-            color_choice_3.setText(fillcolor1);
+            color_choice_3.setText(fillcolor3);
         }
 
         Runnable colors_from_prefs = new Runnable() {
