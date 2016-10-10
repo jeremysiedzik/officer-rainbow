61ec7b1b20ba270ab1678f8934dec94795d547a5
mainphrame <mainphrame@hotmail.com>
2016-10-10 17:30:40 -0400

    confidence pull from Confidence.java

--- app/src/main/java/com/robotagrex/or/officerrainbow/WebSitechecker.java
+++ app/src/main/java/com/robotagrex/or/officerrainbow/WebSitechecker.java
@@ -64,7 +64,7 @@ public class WebSitechecker extends IntentService {
                 this.getSystemService(Context.NOTIFICATION_SERVICE);
     
         PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
-            new Intent(this, UI.class), 0);
+            new Intent(this, Confirm.class), 0);
 
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
